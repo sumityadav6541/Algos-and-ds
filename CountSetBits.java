662fb14b11516f6c017bacf3fed89956f187d2c5
Sumit Yadav <Sumit.Yadav@pb.com>
2016-02-04 13:05:58 +0530

    Count the number of set bits in an Integer

---
+++ CountSetBits.java
@@ -0,0 +1,16 @@
+public class CountSetBits{
+	static int countSetBits(int n){
+		int count=0;
+		while(n>0){
+			n = n&(n-1);
+			count++;
+		}
+		return count;
+	}
+	public static void main(String[] args){
+		System.out.println("number of set Bits in 8 :"+countSetBits(8));
+		System.out.println("number of set Bits in 16 :"+countSetBits(16));
+		System.out.println("number of set Bits in 15 :"+countSetBits(15));
+		System.out.println("number of set Bits in 31 :"+countSetBits(31));
+	}
+}
